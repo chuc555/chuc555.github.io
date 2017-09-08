e34f1c16a8327bf75a74b3ba326d092c422db167
chuc555 <chuc555@newschool.edu>
2017-09-08 10:56:36 -0400

    added jquery
    added jquery effects

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-	console.log("let's party.");
+  $(window).scroll(function() {
+    if ($(document).scrollTop() > 50) {
+      $(".happiness_text").addClass("test");
+    } else {
+      $(".happiness_text").removeClass("test");
+    }
+
+        if ($(document).scrollTop() > 50) {
+      $(".luck_text").addClass("test");
+    } else {
+      $(".luck_text").removeClass("test");
+    }
+  });
 });
