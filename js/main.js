5e77dee3d64faa27d579d6923a1e05b5b5879291
chuc555 <chuc555@newschool.edu>
2017-09-08 10:24:45 -0400

    did a bunch of things to links
    changed links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
 	console.log("let's party.");
-
-$("p").hover(function(){
-    $("#luck").css("background-color", "yellow");
-    }, function(){
-    $("#happiness").css("background-color", "pink");
-});
-
-
 });
