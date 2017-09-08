1443802afc909a71e706a8a72eac51da59c022ca
chuc555 <chuc555@newschool.edu>
2017-09-08 08:59:21 -0400

    jquery hover test
    added a hover state to js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
 	console.log("let's party.");
+
+$("p").hover(function(){
+    $("#luck").css("background-color", "yellow");
+    }, function(){
+    $("#happiness").css("background-color", "pink");
+});
+
+
 });
