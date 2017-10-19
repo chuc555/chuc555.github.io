1d7196e6093fabf11d99cfb519cac03c78766563
chuc555 <chuc555@newschool.edu>
2017-10-19 01:09:00 -0400

    plant loop
    started plant loop

---
+++ js/loop_plant.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+	console.log("let's party.");
+
+	$("#wateringCan").click(function() {
+		var plantHeight = $('#plant').height();
+		var newHeight = plantHeight + 20;
+		$('#plant').css('min-height',newHeight+'px');
+
+		var animationName = 'animated fadeOutDown';
+		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+		$('#droplet').show(1000).addClass(animationName).one(animationEnd, function() {
+				$(this).removeClass(animationName);
+		});
+
+	// end of function
+	}); 
+
+
+
+
+});
