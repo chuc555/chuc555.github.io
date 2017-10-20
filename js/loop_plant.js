0439659c29cc92836af3eb025bd993d17efaca44
chuc555 <chuc555@newschool.edu>
2017-10-20 02:45:05 -0400

    plant loop adjusting
    more tinkering

--- js/loop_plant.js
+++ js/loop_plant.js
@@ -1,25 +1,33 @@
 $(document).ready(function() {
 	console.log("let's party.");
+	
+	$('#flower').hide();
+	// $('#leaf_01').hide();
+	var counter = 0;
 
 	$("#wateringCan").click(function() {
-		var count = 0;
-		
-		if (count == 3) {
-		$('#flower').hide();
-		};
+
+
+		console.log(counter++);
+		if (counter == 10) {
+			$('#plant').css('max-height','150px');
+			// $('#flower').show();
+			$('#leaf_01').show();
+		}
 
 		var plantHeight = $('#plant').height();
-		var newHeight = plantHeight + 20;
+		var newHeight = plantHeight + 10;
 		$('#plant').css('min-height',newHeight+'px');
 
-		var animationName = 'animated fadeOutDown';
-		var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		TweenMax.fromTo('#droplet',1.5, {opacity: 1, y: 0, scale: 0.5}, {opacity: 0, y: 250, scale: 1.5});
 
-		$('#droplet').show(1000).addClass(animationName).one(animationEnd, function() {
-				$(this).removeClass(animationName);
-		});
+		// *****ANOTHER WAY TO ANIMATE DROPLET*****
+		// var animationName = 'animated fadeOutDown';
+		// var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-	
+		// $('#droplet').show(1000).addClass(animationName).one(animationEnd, function() {
+		// 		$(this).removeClass(animationName);
+		// });
 
 	// end of function
 	}); 
