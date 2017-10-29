e40817cc4c8632571a8f4cf5c026afe2e5bad048
chuc555 <chuc555@newschool.edu>
2017-10-29 18:10:51 -0400

    changed index page
    made layout changes, still in progress

--- js/loop_plant.js
+++ js/loop_plant.js
@@ -1,38 +1,48 @@
 $(document).ready(function() {
 	console.log("let's party.");
 	
-	$('#flower').hide();
-	// $('#leaf_01').hide();
+	$('#leaf_01').hide();
+	$('#leaf_02').hide();
+	$('#leaf_03').hide();
+	$('#leaf_04').hide();
+
 	var counter = 0;
 
-	$("#wateringCan").click(function() {
-
-
+	$(".plantLoopBg").click(function() {
 		console.log(counter++);
 		if (counter == 10) {
 			$('#plant').css('max-height','150px');
-			// $('#flower').show();
 			$('#leaf_01').show();
-		}
+		} else if (counter == 12) {
+			$('#leaf_02').show();
+		} else if (counter == 14) {
+			$('#leaf_03').show();
+		} else if (counter == 18) {
+			$('#leaf_04').show();
+		};
 
 		var plantHeight = $('#plant').height();
 		var newHeight = plantHeight + 10;
-		$('#plant').css('min-height',newHeight+'px');
+		$('#plant').delay(1000).css('min-height',newHeight+'px');
 
-		TweenMax.fromTo('#droplet',1.5, {opacity: 1, y: 0, scale: 0.5}, {opacity: 0, y: 250, scale: 1.5});
+		//TweenMax.fromTo('#droplet',1.5, {opacity: 1, y: 0, scale: 0.5}, {opacity: 0, y: 250, scale: 1.5});
+	}); // end of function
+ 	
 
-		// *****ANOTHER WAY TO ANIMATE DROPLET*****
-		// var animationName = 'animated fadeOutDown';
-		// var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	$(document).click(function(e){ 
+	 	var xValue = e.pageX
+ 		var yValue = e.pageY 
+	console.log(xValue +', '+ yValue); 
 
-		// $('#droplet').show(1000).addClass(animationName).one(animationEnd, function() {
-		// 		$(this).removeClass(animationName);
-		// });
+	var windowHeight = $(window).height()
+	console.log(windowHeight)
 
-	// end of function
+	TweenMax.fromTo('#droplet',1.5, {opacity: 1, top: yValue + 45, scale: 0.5, left: xValue - 27}, 
+		{opacity: 0, top: windowHeight - 80, scale: 1.5, left: xValue - 27});
+
+
 	}); 
 
 
 
-
-});
+}); // end of document
