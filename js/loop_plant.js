d47fc211f21fb8acbcd2a8dceae6301a5f9ccd01
chuc555 <chuc555@newschool.edu>
2017-11-03 12:11:13 -0400

    plant heights
    plant height

--- js/loop_plant.js
+++ js/loop_plant.js
@@ -20,27 +20,67 @@ $(document).ready(function() {
 		} else if (counter == 18) {
 			$('#leaf_04').show();
 		};
-
-		var plantHeight = $('#plant').height();
-		var newHeight = plantHeight + 10;
-		$('#plant').delay(1000).css('min-height',newHeight+'px');
-
-		//TweenMax.fromTo('#droplet',1.5, {opacity: 1, y: 0, scale: 0.5}, {opacity: 0, y: 250, scale: 1.5});
 	}); // end of function
  	
 
 	$(document).click(function(e){ 
 	 	var xValue = e.pageX
  		var yValue = e.pageY 
-	console.log(xValue +', '+ yValue); 
+			console.log(xValue +', '+ yValue); 
+		var windowHeight = $(window).height()
+			console.log(windowHeight)
 
-	var windowHeight = $(window).height()
-	console.log(windowHeight)
+		TweenMax.fromTo('#droplet',1.0, 
+			{opacity: 1, top: yValue + 45, scale: 0.5, left: xValue - 27}, 
+			{opacity: 0, top: windowHeight - 80, scale: 1.5, left: xValue - 27});
 
-	TweenMax.fromTo('#droplet',1.5, {opacity: 1, top: yValue + 45, scale: 0.5, left: xValue - 27}, 
-		{opacity: 0, top: windowHeight - 80, scale: 1.5, left: xValue - 27});
+		if ((xValue > 0) && (xValue < 200)) {
+			var plantHeight_02 = $('#plant_02').height();
+			var newHeight_02 = plantHeight_02 + 10;
+			$('#plant_02').delay(1000).css('min-height',newHeight_02+'px');
+		}
 
+		if ((xValue > 201) && (xValue <400)) {
+			var plantHeight_03 = $('#plant_03').height();
+			var newHeight_03 = plantHeight_03 + 10;
+			$('#plant_03').delay(1000).css('min-height',newHeight_03+'px');
+		}
 
+		if ((xValue > 401) && (xValue <600)) {
+			var plantHeight_04 = $('#plant_04').height();
+			var newHeight_04 = plantHeight_04 + 10;
+			$('#plant_04').delay(1000).css('min-height',newHeight_04+'px');
+		}
+
+		if ((xValue > 601) && (xValue <800)) {
+			var plantHeight = $('#plant').height();
+			var newHeight = plantHeight + 10;
+			$('#plant').delay(1000).css('min-height',newHeight+'px');	
+		}
+
+		if ((xValue > 801) && (xValue <970)) {
+			var plantHeight_05 = $('#plant_05').height();
+			var newHeight_05 = plantHeight_05 + 10;
+			$('#plant_05').delay(1000).css('min-height',newHeight_05+'px');	
+		}
+
+		if ((xValue > 971) && (xValue <1100)) {
+			var plantHeight_06 = $('#plant_06').height();
+			var newHeight_06 = plantHeight_06 + 10;
+			$('#plant_06').delay(1000).css('min-height',newHeight_06+'px');	
+		}
+
+		if ((xValue > 1101) && (xValue <1300)) {
+			var plantHeight_07 = $('#plant_07').height();
+			var newHeight_07 = plantHeight_07 + 10;
+			$('#plant_07').delay(1000).css('min-height',newHeight_07+'px');	
+		}
+
+	var currentHeight = $('#plant_06').css("height")
+	console.log(currentHeight);	
+
+	Cookies.set('plantState', currentHeight, {expires: 10});
+	
 	}); 
 
 
